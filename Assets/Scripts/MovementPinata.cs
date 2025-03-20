1b218a4247ed54f5954c6e1bc94de5e3e5c69993
Liad Nagi <liadnagi@gmail.com>
2025-03-20 13:35:11 +0200

    Update movement Pinata

--- Assets/Scripts/MovementPinata.cs
+++ Assets/Scripts/MovementPinata.cs
@@ -2,24 +2,28 @@ using UnityEngine;
 
 public class MovementPinata : MonoBehaviour
 {
-    [Header("Pendulum Movement Settings")]
-    [SerializeField] private float swingAmplitude = 30f;  // Maximum rotation in degrees
-    [SerializeField] private float swingSpeed = 1f;       // Speed of the swing
+    // [Header("Pendulum Movement Settings")]
+    // [SerializeField] private float swingAmplitude = 30f;  // Maximum rotation in degrees
+    // [SerializeField] private float swingSpeed = 1f;       // Speed of the swing
 
     private float initialZRotation;
 
     private void Start()
     {
         // Save the initial local rotation around the Z axis
-        initialZRotation = transform.localEulerAngles.z;
+        // initialZRotation = transform.localEulerAngles.z;
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        // For a small push:
+        rb.AddTorque(2f, ForceMode2D.Impulse);
     }
 
     private void Update()
     {
         // Calculate a new rotation around the Z axis using a sinusoidal function
-        float angle = initialZRotation + swingAmplitude * Mathf.Sin(Time.time * swingSpeed);
+        // float angle = initialZRotation + swingAmplitude * Mathf.Sin(Time.time * swingSpeed);
 
-        // Apply the rotation to the rope
-        transform.localRotation = Quaternion.Euler(0f, 0f, angle);
+        // // Apply the rotation to the rope
+        // transform.localRotation = Quaternion.Euler(0f, 0f, angle);
     }
 }
